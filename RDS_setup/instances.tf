62203f68ee60cac49c344d874ad7077492fd7654
morganbrow <morgan.brown@cloudreach.com>
2022-05-02 10:40:14 -0400

    init commit

---
+++ RDS_setup/instances.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "my_public_server" {
+  ami                    = data.aws_ami.aws_basic_linux.id
+  instance_type          = var.ec2_type
+  subnet_id              = data.aws_subnet.canary-vpc-public-a.id
+  vpc_security_group_ids = [aws_security_group.my_public_app_sg.id]
+  key_name               = var.my_keypair
+  iam_instance_profile   = aws_iam_instance_profile.dashboard_server_profile.id
+
+  tags = {
+    Name = "public_server"
+  }
+}
---
+++ RDS_setup/terraform.tfvars
@@ -0,0 +1,8 @@
+# ASSIGNING VALUE TO VARIABLES
+aws_owner_id                = "137112412989"
+aws_ami_name                = "amzn2-ami-kernel-5.10-hvm*"
+vpc_name                    = "canary-vpc"
+public_subnet_name          = "canary-vpc-public-a"
+private_subnet_name         = "canary-data-a"
+number_of_instances         = 1
+number_of_private_instances = 1
