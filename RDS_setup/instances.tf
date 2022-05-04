f40112648aa81070ce443b9e4b2dd88fef84bc7a
morganbrow <morgan.brown@cloudreach.com>
2022-05-04 06:53:26 -0700

    init commit

--- RDS_setup/instances.tf
+++ RDS_setup/instances.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "my_public_server" {
+resource "aws_instance" "blue_server" {
   ami                    = data.aws_ami.aws_basic_linux.id
   instance_type          = var.ec2_type
   subnet_id              = data.aws_subnet.canary-vpc-public-a.id
@@ -7,6 +7,18 @@ resource "aws_instance" "my_public_server" {
   iam_instance_profile   = aws_iam_instance_profile.dashboard_server_profile.id
 
   tags = {
-    Name = "public_server"
+    Name = "blue_server"
+  }
+}
+resource "aws_instance" "green_server" {
+  ami                    = data.aws_ami.aws_basic_linux.id
+  instance_type          = var.ec2_type
+  subnet_id              = data.aws_subnet.canary-vpc-public-a.id
+  vpc_security_group_ids = [aws_security_group.my_public_app_sg.id]
+  key_name               = var.my_keypair
+  iam_instance_profile   = aws_iam_instance_profile.dashboard_server_profile.id
+
+  tags = {
+    Name = "green_server"
   }
 }
